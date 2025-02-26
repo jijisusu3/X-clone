1ff46bea80cc3228df482fa925834d7f804963ce
JISU KIM <97648027+jijisusu3@users.noreply.github.com>
2025-02-26 17:17:49 -0500

    FE: UI design completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
 import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
 import LoginPage from "./pages/auth/login/LoginPage.jsx";
+import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from "./components/common/RightPanel.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
